2d1b5d014731bf20ce78a3e34cce7212856347a2
Wojciech Kula <wojtekkula33@gmail.com>
2022-02-19 07:45:02 +0100

    DSA-4: picture of the day layout

--- app/src/main/java/com/wojciechkula/deepskyapp/presentation/favouritepictures/FavouritePictures.kt
+++ app/src/main/java/com/wojciechkula/deepskyapp/presentation/favouritepictures/FavouritePictures.kt
@@ -1,10 +1,10 @@
 package com.wojciechkula.deepskyapp.presentation.favouritepictures
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.wojciechkula.deepskyapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,7 +19,8 @@ class FavouritePictures : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+//        activity?.window?.statusBarColor= activity?.let { ContextCompat.getColor(it, R.color.red_500) }!!
+//        activity?.setTheme(R.style.Theme_DeepskyApp_FavouritePictures)
         return inflater.inflate(R.layout.fragment_favourite_pictures, container, false)
     }
 
