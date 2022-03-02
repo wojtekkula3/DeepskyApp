3c64b2807023305687a8f3d9b563add4c4db080f
Wojciech Kula <wojtekkula33@gmail.com>
2022-03-02 22:13:50 +0100

    DSA-15: navigation layout improvements

--- app/src/main/java/com/wojciechkula/deepskyapp/MainActivity.kt
+++ app/src/main/java/com/wojciechkula/deepskyapp/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.wojciechkula.deepskyapp
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
@@ -23,13 +28,38 @@ class MainActivity : AppCompatActivity() {
             .setLaunchSingleTop(true)
             .setEnterAnim(R.anim.enter_right_to_left)
             .setExitAnim(R.anim.exit_right_to_left)
+            .setPopEnterAnim(R.anim.enter_left_to_right)
+            .setPopExitAnim(R.anim.exit_left_to_right)
             .build()
 
         val optionsAPOD = NavOptions.Builder()
             .setLaunchSingleTop(true)
             .setEnterAnim(R.anim.enter_left_to_right)
             .setExitAnim(R.anim.exit_left_to_right)
+            .setPopEnterAnim(R.anim.enter_right_to_left)
+            .setPopExitAnim(R.anim.exit_right_to_left)
             .build()
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.pictureOfTheDay -> {
+                    val xrp = resources.getXml(R.drawable.bottom_nav_selector)
+                    val colorStateList = ColorStateList.createFromXml(resources, xrp, theme)
+                    bottomNavigationView.itemIconTintList = colorStateList
+                    bottomNavigationView.itemTextColor = colorStateList
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.favouritePictures -> {
+                    val xrp = resources.getXml(R.drawable.bottom_nav_selector_favourite)
+                    val colorStateList = ColorStateList.createFromXml(resources, xrp, theme)
+                    bottomNavigationView.itemIconTintList = colorStateList
+                    bottomNavigationView.itemTextColor = colorStateList
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.pictureDetails -> bottomNavigationView.visibility = View.GONE
+            }
+        }
+        bottomNavigationView.setupWithNavController(navController)
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
@@ -43,4 +73,5 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
+
 }
